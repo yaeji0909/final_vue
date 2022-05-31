aa379b7a2e64b9288fc6f65469006955486d1ce7
Yaeji <yaeji0909@gmail.com>
2022-05-31 14:31:12 +0900

    modify

--- vue-til/src/store/index.js
+++ vue-til/src/store/index.js
@@ -37,7 +37,6 @@ export default new Vuex.Store({
   actions: {
     async LOGIN({ commit }, userData) {
       const { data } = await loginUser(userData);
-      console.log(data.token);
       commit('setToken', data.token);
       commit('setUsername', data.user.username);
       saveAuthToCookie(data.token);
